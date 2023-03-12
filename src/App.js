35e8c88e390340d6601a153961ec04852da37c69
Akmoor Abdirashid <akmoor.abdirashid.ch@gmail.com>
2023-03-13 01:47:10 +0400

    Fixing the last errors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Login from './components/Login'
 import Header from './components/Header'
 import Home from './components/Home';
 import Detail from './components/Detail';
-import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -18,36 +17,5 @@ function App() {
     </BrowserRouter>
   )
 }
-
-// import './App.css';
-// import {BrowserRouter as Router, Switch, Route, BrowserRouter} from "react-router-dom";
-// import Login from './components/Login'
-// import Header from './components/Header'
-// import Home from './components/Home';
-// import Detail from './components/Detail';
-// import { useEffect } from 'react';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <div className="App">
-//         <Router>
-//           <Header/>
-//           <Switch>
-//             <Route exact path='/'>
-//               <Login/>
-//             </Route>
-//             <Route exact path='/home'>
-//               <Home />
-//             </Route>
-//             <Route path='/detail/:id'>
-//               <Detail/>
-//             </Route>
-//           </Switch>
-//         </Router>
-//       </div>
-//     </BrowserRouter>
-//   );
-// }
 
 export default App;
